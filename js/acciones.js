c8a512c1824472ab36f8ddd6616d22427a13e792
master23451 <2119300464@soy.utj.edu.mx>
2022-06-18 16:11:51 -0500

    -Primera carga de los archivos version 1.0 Sin cambios importantes mas que la comentacion de distintas lineas de codigo que estan de mas.

--- js/acciones.js
+++ js/acciones.js
@@ -89,13 +89,12 @@ function readImage(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
         reader.onload = function (e) {
-            $('#imgPreview').attr('src', e.target.result); // Renderizamos la imagen
+            $('#imgPreview').attr('src', e.target.result);
         }
         reader.readAsDataURL(input.files[0]);
     }
 }
 
 $("#file-selector").change(function () {
-    //Código a ejecutar cuando se detecta un cambio de archivO
     readImage(this);
 });
